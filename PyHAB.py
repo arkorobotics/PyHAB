642960ab07383cb3a7b678f08467f47b83fc5f35
Arko <dotuplink@gmail.com>
2016-09-01 20:54:28 -0700

    Working HAB!
    Working PyHAB! Now to get it to identify as a payload…

--- PyHAB.py
+++ PyHAB.py
@@ -13,6 +13,42 @@ from time import sleep, time
 
 state = "RESET";
 
+#GGA          Global Positioning System Fix Data
+#123519       Fix taken at 12:35:19 UTC
+#4807.038,N   Latitude 48 deg 07.038' N
+#01131.000,E  Longitude 11 deg 31.000' E
+#1            Fix quality: 0 = invalid
+#				1 = GPS fix (SPS)
+#				2 = DGPS fix
+#				3 = PPS fix
+#				4 = Real Time Kinematic
+#				5 = Float RTK
+#				6 = estimated (dead reckoning) (2.3 feature)
+#				7 = Manual input mode
+#				8 = Simulation mode
+#08           Number of satellites being tracked
+#0.9          Horizontal dilution of position
+#545.4,M      Altitude, Meters, above mean sea level
+#46.9,M       Height of geoid (mean sea level) above WGS84
+#ellipsoid
+#(empty field) time in seconds since last DGPS update
+#(empty field) DGPS station ID number
+#*47          the checksum data, always begins with *
+#nema_msg = "$GPGGA,123519,4807.038,N,01131.000,E,1,08,0.9,545.4,M,46.9,M,,*47"
+nema_msg = "" #"$GPGGA,,,,,,,,,,,,,,*47"
+latitude = ""
+longitude = ""
+altitude = ""
+
+# no alt sample packet: 3xL34.091,-118.067,T23X144716.00:N6ARA[PyHAB]
+# full sample packet: 3oL34.091,-118.067,297.4T24X144937.00:N6ARA[PyHAB]
+data_count = ["a" , "b" , "c" , "d" , "e" , "f" , "g" , "h" ,
+"i" , "j" , "k" , "l" , "m" , "n" , "o" , "p" ,
+"q" , "r" , "s" , "t" , "u" , "v" , "w" , "x" ,
+"y" , "z"]
+
+data_idx = 0
+
 # Initialize perpherial variables
 uart = UART(1, 9600)
 #spi = SPI(1, SPI.MASTER, baudrate=1000000, polarity=0, phase=0, firstbit=SPI.MSB, crc=None)
@@ -69,45 +105,96 @@ def init():
 def gps_acq():
 	print ("Acquiring GPS data")
 
-	# Default GPS Data is 8 lines
-	print (uart.readline())	#GNVTG
-	print (uart.readline()) #GNGGA
-	print (uart.readline()) #GNGSA
-	print (uart.readline()) #GNGSA
-	print (uart.readline()) #GPGSV
-	print (uart.readline()) #GPGSV
-	print (uart.readline()) #GNGLL
-	print (uart.readline()) #GNRMC
+	global nema_msg
 
+	# Default GPS Data is 8 lines
+	nema_sentence_flag = 0
+	while (nema_sentence_flag != 1):
+		nema_msg = uart.readline() 
+		print (nema_msg)
+		if (nema_msg[3:6] == bytearray(b'GGA')):
+			nema_sentence_flag = 1
+
+	print ("message found: %s" % nema_msg)
 	return "PARSE_GPS"
 
 def parse_gps():
 	print ("Parsing GPS data")
 
-	print ("Temperature: %d" % rfm69.readTemp())
+	global nema_msg
+	global time
+	global latitude
+	global longitude
+	global altitude
 
-	#print ("RSSI: %d" % rfm69.sampleRssi())
+	nema_arr = str(nema_msg).split(',')
+	print (nema_arr)
+	time = nema_arr[1]
+
+	latitude_sign = ''
+	longitude_sign = ''
+
+	if(nema_arr[3] == 'N'): 
+		latitude_sign = ''
+	elif(nema_arr[3] == 'S'):
+		latitude_sign = '-'
+
+	if(len(nema_arr[2]) > 0):
+		latitude = "%s%.3f" % (latitude_sign, float(nema_arr[2])/100)
+
+	if(nema_arr[5] == 'E'): 
+		longitude_sign = ''
+	elif(nema_arr[5] == 'W'):
+		longitude_sign = '-'
+
+	if(len(nema_arr[2]) > 0):
+		longitude = "%s%.3f" % (longitude_sign, float(nema_arr[4])/100)
+
+	altitude = nema_arr[9]
+
 	return "TRANSMIT"
 
 def transmit():
+	print (" ")
 	print ("Transmitting position and telemetry")
 
-	data = bytearray(b"3iL34.154,-118.128V589:N6ARA[PyHAB]")
+	rfmtemp = rfm69.readTemp()
+
+	print ("Temperature: %d" % rfmtemp)
+	print ("RSSI: %d" % rfm69.sampleRssi())
+	
+	global time
+	global latitude
+	global longitude
+	global altitude
+
+	global data_count
+	global data_idx
+
+	if(data_idx >= 0 and data_idx < 25):
+		data_idx += 1
+	else:
+		data_idx = 0
+
+	print ("dataidx: %d" % data_idx)
+	ukhasnode_str = "3%sL%s,%s,%sT%dX%s:N6ARA[PyHAB]" % (data_count[data_idx],latitude,longitude,altitude,rfmtemp,time)
+
+	data = bytearray(ukhasnode_str)
 
 	rfm69.send(data, len(data), 19)
-
+	print (ukhasnode_str)
 	print (" ")
 	print (" ")
 	sleep(1)
 	print ("Check rx buf")
 	rfm69.set_mode(registers["RFM69_MODE_RX"])
 	rfm69.setLnaMode(registers["RF_TESTLNA_SENSITIVE"])
-	rfm69.checkRx()
-	print (rfm69.recv())
+	#rfm69.checkRx()
+	#print (rfm69.recv())
 	rfm69.clearFifo()
 	print (" ")
-	#return "GPS_ACQ"
-	return "PARSE_GPS"
+
+	return "GPS_ACQ"
 
 states = {
 		"FAULT": fault,
